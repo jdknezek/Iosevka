cf4b196e89ec8ab69523825e1a725d46060fa5b5
Jonathan Knezek <jdknezek@gmail.com>
2021-05-31 13:58:54 -0500

    Don't build -super.ttc by default

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -o errexit -o errtrace -o nounset -o pipefail
 
 plans=''
 for family in flat hyperlegible; do
-    for spacing in '' -term -fixed -proportional -super; do
+    for spacing in '' -term -fixed -proportional; do
         plans="$plans super-ttc::iosevka-${family}${spacing}"
     done
 done
