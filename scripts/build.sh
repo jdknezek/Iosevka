47e2dfc92a81125b4586a1803c80dfdb67d7e278
Jonathan Knezek <jonathan.knezek@ampthink.com>
2024-08-12 10:39:19 -0500

    Customizations

---
+++ scripts/build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+npm run build -- --jCmd=12 super-ttc::IosevkaCustom super-ttc::IosevkaPropCustom super-ttc::IosevkaAileCustom super-ttc::IosevkaEtoileCustom super-ttc::Hypersevka super-ttc::HypersevkaProp
+# npm run build -- --jCmd=12 super-ttc::IosevkaCustom super-ttc::IosevkaPropCustom super-ttc::IosevkaAileCustom super-ttc::IosevkaEtoileCustom
+# npm run build -- --jCmd=12 super-ttc::Hypersevka super-ttc::HypersevkaProp
