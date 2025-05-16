b2f32f76f0731dd58807f1a724223469b89e21b8
Jonathan Knezek <jonathan.knezek@ampthink.com>
2025-05-16 09:30:17 -0500

    Add Iosefka Mono (SF Mono style)

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 cd dist
-7zr a -mx=9 IosevkaCustom.7z ./.super-ttc/Iosevka*.ttc && mv IosevkaCustom.7z /mnt/c/Users/jonathan.knezek/Dropbox/fonts/
+7zr a -mx=9 IosevkaCustom.7z ./.super-ttc/Iosevka*Custom.ttc && mv IosevkaCustom.7z /mnt/c/Users/jonathan.knezek/Dropbox/fonts/
 7zr a -mx=9 Hypersevka.7z ./.super-ttc/Hypersevka*.ttc && mv Hypersevka.7z /mnt/c/Users/jonathan.knezek/Dropbox/fonts/
+7zr a -mx=9 IosevkaTxtr.7z ./.super-tcc/IosevkaTxtr.ttc && mv IosevkaTxtr.7z /mnt/c/Users/jonathan.knezek/Dropbox/fonts/
+7zr a -mx=9 IosefkaMono.7z ./.super-ttc/IosefkaMono.ttc && mv IosefkaMono.7z /mnt/c/Users/jonathan.knezek/Dropbox/fonts/
