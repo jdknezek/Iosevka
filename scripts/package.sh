6b023fec5e305b45afe0334f386ee4374443b361
Jonathan Knezek <jonathan.knezek@ampthink.com>
2025-11-10 08:57:45 -0600

    Restructure generate-plans; add Iosevka Sans

--- scripts/package.sh
+++ scripts/package.sh
@@ -4,3 +4,4 @@ cd dist
 7zr a -mx=9 Hypersevka.7z ./.super-ttc/Hypersevka*.ttc && mv Hypersevka.7z /mnt/c/Users/jonathan.knezek/Dropbox/fonts/
 7zr a -mx=9 IosefkaMono.7z ./.super-ttc/IosefkaMono.ttc && mv IosefkaMono.7z /mnt/c/Users/jonathan.knezek/Dropbox/fonts/
 7zr a -mx=9 IosevkaJDK.7z ./.super-ttc/IosevkaJDK.ttc && mv IosevkaJDK.7z /mnt/c/Users/jonathan.knezek/Dropbox/fonts/
+7zr a -mx=9 IosevkaSans.7z ./.super-ttc/IosevkaSans.ttc && mv IosevkaSans.7z /mnt/c/Users/jonathan.knezek/Dropbox/fonts/
