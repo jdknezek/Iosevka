bfffb149f5dee0a672c3266cb980e02c91da0b77
Jonathan Knezek <jonathan.knezek@ampthink.com>
2026-03-01 23:14:00 -0600

    Delete .ttc files after packaging

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd dist
-7zr a -mx=9 IosevkaTxtr.7z ./.super-ttc/*Txtr.ttc && mv IosevkaTxtr.7z /mnt/c/Users/jonathan.knezek/Dropbox/fonts/
-7zr a -mx=9 Hypersevka.7z ./.super-ttc/Hypersevka*.ttc && mv Hypersevka.7z /mnt/c/Users/jonathan.knezek/Dropbox/fonts/
-7zr a -mx=9 IosefkaMono.7z ./.super-ttc/IosefkaMono.ttc && mv IosefkaMono.7z /mnt/c/Users/jonathan.knezek/Dropbox/fonts/
-7zr a -mx=9 IosevkaJDK.7z ./.super-ttc/IosevkaJDK.ttc && mv IosevkaJDK.7z /mnt/c/Users/jonathan.knezek/Dropbox/fonts/
-7zr a -mx=9 IosevkaSans.7z ./.super-ttc/IosevkaSans.ttc && mv IosevkaSans.7z /mnt/c/Users/jonathan.knezek/Dropbox/fonts/
+7zr a -mx=9 -sdel IosevkaTxtr.7z ./.super-ttc/*Txtr.ttc && mv IosevkaTxtr.7z /mnt/c/Users/jonathan.knezek/Dropbox/fonts/
+7zr a -mx=9 -sdel Hypersevka.7z ./.super-ttc/Hypersevka*.ttc && mv Hypersevka.7z /mnt/c/Users/jonathan.knezek/Dropbox/fonts/
+7zr a -mx=9 -sdel IosefkaMono.7z ./.super-ttc/IosefkaMono.ttc && mv IosefkaMono.7z /mnt/c/Users/jonathan.knezek/Dropbox/fonts/
+7zr a -mx=9 -sdel IosevkaJDK.7z ./.super-ttc/IosevkaJDK.ttc && mv IosevkaJDK.7z /mnt/c/Users/jonathan.knezek/Dropbox/fonts/
+7zr a -mx=9 -sdel IosevkaSans.7z ./.super-ttc/IosevkaSans.ttc && mv IosevkaSans.7z /mnt/c/Users/jonathan.knezek/Dropbox/fonts/
